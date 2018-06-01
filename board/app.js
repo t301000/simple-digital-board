4aaff0e246ad47def5bb4d44cf4a0dd701745794
林士立 <t301000@gmail.com>
2018-06-02 00:09:11 +0800

    新增對預設項目之處理

--- board/app.js
+++ board/app.js
@@ -31,12 +31,17 @@ const db = firebase.firestore();
   function getUrls(id) {
     db.collection(`channels/${id}/resources`)
       .onSnapshot(snapshot => {
+        const defaultItem = getDefaultResource();
         snapshot.docChanges().forEach(
           ({type, doc}) => {
             const obj = {id: doc.id, ...doc.data()};
             switch (type) {
               case 'added': // 初次載入或新增
                 urls = [...urls, obj];
+                // 設定預設，避免 localStorage 與 firestore 不一致
+                if (defaultItem && defaultItem.id === doc.id) {
+                  setDefaultResource(obj);
+                }
                 break;
               case 'modified': // 修改
                 if (playing && playing.id === doc.id) {
@@ -44,20 +49,32 @@ const db = firebase.firestore();
                   playing = obj;
                   play();
                 }
+
+                // 修改到預設，則更新
+                if (defaultItem && defaultItem.id === doc.id) {
+                  setDefaultResource(obj);
+                }
                 const idx = urls.findIndex(item => item.id === doc.id);
                 urls = [...urls.slice(0, idx), obj, ...urls.slice(idx + 1)];
                 break;
               case 'removed': // 刪除
                 // console.log(`id: ${doc.id} removed`);
                 urls = urls.filter(item => item.id !== doc.id);
+
+                // 刪除到預設，則移除
+                if (defaultItem && defaultItem.id === doc.id) {
+                  localStorage.removeItem('default');
+                }
                 break;
             }
           }
         );
 
+        // 還沒播放，則開始播放
         if (!playing) {
           play();
         }
+        // 產生選單
         generateMenu();
       });
   }
@@ -91,11 +108,16 @@ const db = firebase.firestore();
   }
 
   // 選單 click handler
+  // 選單項目為動態由 template 產生
+  // 未出現在 DOM 之前無法設定 事件監聽
+  // 因此改採由父層監聽處理
   function menuContentClickHandler(event) {
+    // 實際發出事件之物件
     const srcElm = event.target;
+    // 父層物件
     const wrapper = srcElm.parentNode;
 
-    // 按下按鈕
+    // 按下按鈕，變更當前播放
     if (srcElm.className.indexOf('resource-button') !== -1) {
       // 更新正在播放之資源
       playing = {id: wrapper.dataset['id'], url: wrapper.dataset['url']};
@@ -103,10 +125,10 @@ const db = firebase.firestore();
       updatePlayingMark();
     }
 
-    // 按下 radio
+    // 按下 radio，變更預設
     if (srcElm.className.indexOf('radio') !== -1) {
       // 設為預設值
-      localStorage.setItem('default', JSON.stringify({id: wrapper.dataset['id'], url: wrapper.dataset['url']}));
+      setDefaultResource({id: wrapper.dataset['id'], url: wrapper.dataset['url']});
       updateDefaultMark(wrapper.dataset['id']);
     }
   }
@@ -159,15 +181,23 @@ const db = firebase.firestore();
     return JSON.parse(localStorage.getItem('default'));
   }
 
+  // 儲存預設播放資源
+  function setDefaultResource(obj) {
+    localStorage.setItem('default', JSON.stringify(obj));
+  }
+
   // 監聽 firestore document 變化
   // 由遠端控制
-  // 第一次接收到 執行第一次播放
-  // 第二次以後接收到 執行重新載入播放
+  // 第一次接收到為剛載入時 執行第一次播放
+  // 第二次以後接收到為遠端控制 執行重新載入當前播放
   function listenForReload(id) {
     if (!id) return false;
 
     db.doc(`channels/${id}`)
-      .onSnapshot(doc => play());
+      .onSnapshot(doc => {
+        play();
+        // console.log(doc.get('reloadAt'));
+      });
   }
 
 })(db);
