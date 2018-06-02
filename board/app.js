2d8af0def679e00bd52accb741dc39ed87e62b8e
林士立 <t301000@gmail.com>
2018-06-02 21:51:01 +0800

    調整並清理

--- board/app.js
+++ board/app.js
@@ -4,7 +4,8 @@ const db = firebase.firestore();
 ;(function(db) {
 
   let urls = [];
-  let playing = getDefaultResource();
+  let playing = null;
+  let defaultResource = null;
 
   const template = document.querySelector('#resource-item');
   const menuContent = document.querySelector('.content');
@@ -18,8 +19,15 @@ const db = firebase.firestore();
 
   db.collection('channels').where('name', '==', department).limit(1).get()
     .then(docs => docs.forEach(doc => {
-      getUrls(doc.id);
-      listenForReload(doc.id);
+      getUrls(doc.id); // 取得資源清單並產生選單
+      iframe.setAttribute('data-id', doc.id);
+
+      // TODO: maybe setTimeout....
+      listenForReload(doc.id); // 監聽來自遠端的重載指示
+      // listenForSetDefault(doc.id); // 監聽來自遠端的設定預設資源指示
+      // listenForSetPlaying(doc.id); // 監聽來自遠端的設定目前播放資源指示
+      setTimeout(() => listenForSetDefault(doc.id), 1000);
+      setTimeout(() => listenForSetPlaying(doc.id), 3000);
     }));
 
 
@@ -31,28 +39,24 @@ const db = firebase.firestore();
   function getUrls(id) {
     db.collection(`channels/${id}/resources`)
       .onSnapshot(snapshot => {
-        const defaultItem = getDefaultResource();
+        // const defaultItem = getDefaultResource();
         snapshot.docChanges().forEach(
           ({type, doc}) => {
             const obj = {id: doc.id, ...doc.data()};
             switch (type) {
               case 'added': // 初次載入或新增
                 urls = [...urls, obj];
-                // 設定預設，避免 localStorage 與 firestore 不一致
-                if (defaultItem && defaultItem.id === doc.id) {
-                  setDefaultResource(obj);
-                }
                 break;
 
               case 'modified': // 修改
                 if (playing && playing.id === doc.id) {
                   // 修改到目前播放資源
-                  playing = obj;
+                  setPlayingResource(obj);
                   play();
                 }
 
                 // 修改到預設，則更新
-                if (defaultItem && defaultItem.id === doc.id) {
+                if (defaultResource && defaultResource.id === doc.id) {
                   setDefaultResource(obj);
                 }
                 const idx = urls.findIndex(item => item.id === doc.id);
@@ -65,28 +69,21 @@ const db = firebase.firestore();
 
                 if (playing && playing.id === doc.id) {
                   // 刪除到目前播放資源
-                  playing = null;
-                  iframe.setAttribute('src', '');
+                  setPlayingResource(null);
+                  play();
                 }
 
                 // 刪除到預設，則移除
-                if (defaultItem && defaultItem.id === doc.id) {
-                  localStorage.removeItem('default');
+                if (defaultResource && defaultResource.id === doc.id) {
+                  setDefaultResource(null);
                 }
                 break;
             }
           }
         );
 
-        // 還沒播放，則開始播放
-        if (!playing) {
-          play();
-        }
         // 產生選單
         generateMenu();
-        // 更新 ui 標記
-        updateDefaultMark();
-        updatePlayingMark();
       });
   }
 
@@ -94,27 +91,34 @@ const db = firebase.firestore();
   function generateMenu() {
     // console.log(urls);
     menuContent.innerHTML = '';
-    urls.forEach(obj => {
+    urls.forEach((obj, idx)=> {
       const wrapper = template.content.querySelector('.resource');
       const btn = template.content.querySelector('.resource-button');
       btn.textContent = obj.name;
-      wrapper.setAttribute('data-id', obj.id);
-      wrapper.setAttribute('data-url', obj.url);
+      wrapper.setAttribute('data-id', obj.id); // for ui 標記
+      wrapper.setAttribute('data-idx', idx.toString()); // for 設定 default and playing
 
       // 附加一個選項至 DOM
       const clone = document.importNode(template.content, true);
       menuContent.appendChild(clone);
-  
-      // 更新 ui 標記
-      // updateDefaultMark();
-      // updatePlayingMark();
     });
   }
 
   // 執行播放
+  // 播放指定資源
+  // 第一次載入時播放預設資源
   function play() {
     if (playing !== null) {
       iframe.setAttribute('src', playing.url);
+      updatePlayingMark();
+    } else {
+      // 第一次載入時播放預設資源
+      if (defaultResource !== null) {
+        playing = defaultResource;
+        play();
+      } else {
+        iframe.setAttribute('src', '');
+      }
     }
   }
 
@@ -129,43 +133,34 @@ const db = firebase.firestore();
     const wrapper = srcElm.parentNode;
 
     // 按下按鈕，變更當前播放
-    if (srcElm.className.indexOf('resource-button') !== -1) {
-      // 更新正在播放之資源
-      playing = {id: wrapper.dataset['id'], url: wrapper.dataset['url']};
+    if (srcElm.classList.contains('resource-button') && !srcElm.classList.contains('playing')) {
+      // 更新正在播放之資源，並播放
+      setPlayingResource(urls[wrapper.dataset['idx']]);
       play();
-      updatePlayingMark();
     }
 
     // 按下 radio，變更預設
-    if (srcElm.className.indexOf('radio') !== -1) {
-      // 設為預設值
-      setDefaultResource({id: wrapper.dataset['id'], url: wrapper.dataset['url']});
-      updateDefaultMark(wrapper.dataset['id']);
+    if (srcElm.classList.contains('radio') && !srcElm.classList.contains('default')) {
+      // 設為預設值，更新至 firestore
+      db.doc(`channels/${iframe.dataset['id']}/actions/setDefault`)
+        .set({id: wrapper.dataset['id']})
+        .then(() => console.log(`set default id: ${wrapper.dataset['id']}`))
+        .catch(err => console.log(err));
     }
   }
 
   // 更新是否為預設播放項目之標記
-  function updateDefaultMark(id) {
+  function updateDefaultMark() {
     const radios = document.querySelectorAll('.radio');
-    // 未傳入 id，則檢查是否有預設
-    if (!id) {
-      const defaultResource = getDefaultResource();
-      if (!defaultResource) {
-        radios.forEach(radio => radio.classList.remove('default'));
-        return false;
-      }
-      id = defaultResource.id;
+
+    if (!defaultResource) {
+      radios.forEach(radio => radio.classList.remove('default'));
+      return false;
     }
 
     radios.forEach(radio => {
       const wrapper = radio.parentNode;
-      if (wrapper.dataset['id'] === id) {
-        if (!radio.classList.contains('default')) {
-          radio.classList.add('default');
-        }
-      } else {
-        radio.classList.remove('default');
-      }
+      wrapper.dataset['id'] === defaultResource.id ? radio.classList.add('default') : radio.classList.remove('default');
     });
   }
 
@@ -180,35 +175,60 @@ const db = firebase.firestore();
 
     btns.forEach(btn => {
       const wrapper = btn.parentNode;
-      if (wrapper.dataset['id'] === playing.id) {
-        if (!btn.classList.contains('playing')) {
-          btn.classList.add('playing');
-        }
-      } else {
-        btn.classList.remove('playing');
-      }
+      wrapper.dataset['id'] === playing.id ? btn.classList.add('playing') : btn.classList.remove('playing');
     });
   }
   
-  // 取得預設播放資源
-  function getDefaultResource() {
-    return JSON.parse(localStorage.getItem('default'));
+  // 設定預設播放資源
+  function setDefaultResource(obj) {
+    defaultResource = obj ? obj : null;
+    updateDefaultMark();
   }
 
-  // 儲存預設播放資源
-  function setDefaultResource(obj) {
-    localStorage.setItem('default', JSON.stringify(obj));
+  // 設定目前播放資源
+  function setPlayingResource(obj) {
+    playing = obj;
   }
 
   // 監聽 firestore document 變化
-  // 由遠端控制
-  // 第一次接收到為剛載入時 執行第一次播放
+  // 由遠端控制重載
+  // 第一次接收到為剛載入時 略過
   // 第二次以後接收到為遠端控制 執行重新載入當前播放
   function listenForReload(id) {
     if (!id) return false;
 
+    let count = 0;
     db.doc(`channels/${id}/actions/reload`)
       .onSnapshot(doc => {
+        count++;
+        if (count > 1) play();
+      });
+  }
+
+  // 監聽 firestore document 變化
+  // 由遠端控制設定預設資源
+  function listenForSetDefault(id) {
+    if (!id) return false;
+
+    db.doc(`channels/${id}/actions/setDefault`)
+      .onSnapshot(doc => {
+        const obj = urls.find(item => item.id === doc.get('id'));
+        setDefaultResource(obj);
+      });
+  }
+
+  // 監聽 firestore document 變化
+  // 由遠端控制設定目前播放資源
+  function listenForSetPlaying(id) {
+    if (!id) return false;
+
+    db.doc(`channels/${id}/actions/setPlaying`)
+      .onSnapshot(doc => {
+        const obj = urls.find(item => item.id === doc.get('id'));
+
+        if (playing && obj) {
+          setPlayingResource(obj);
+        }
         play();
       });
   }
