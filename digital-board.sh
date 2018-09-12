878d34cdfe22b47ec6191b181b8f72fd2eb5c865
林士立 <t301000@gmail.com>
2018-09-12 10:19:41 +0800

    調整參數 --incognito

--- digital-board.sh
+++ digital-board.sh
@@ -8,7 +8,8 @@ url='simple-digital-board/board/index.html'
 # 參考：
 #     https://ssk7833.github.io/blog/2016/08/02/setup-Google-Chrome-as-kiosk-application-settings/
 # 可按 F11 離開全螢幕
-/usr/bin/chromium-browser --start-fullscreen $url --user-data-dir="chrome-user-data-dir" --disable-web-security --test-type --disable-translate
+#/usr/bin/chromium-browser --start-fullscreen $url --user-data-dir="chrome-user-data-dir" --disable-web-security --test-type --disable-translate
+/usr/bin/chromium-browser --incognito --start-fullscreen $url --user-data-dir="chrome-user-data-dir" --disable-web-security --test-type --disable-translate
 # 運行 kiosk 模式
 # 開啟時直接進入全螢幕模式，且無法利用 F11 跟 ESC 來離開全螢幕模式，可以用 ALT + F4 或 CTRL + W 來關閉。
 # 在參數列加上 --kiosk 即可。
